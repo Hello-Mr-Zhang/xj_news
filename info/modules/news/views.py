505d5226cedc265cb662bd57e18ce85d1431f733
Hello-Mr-Zhang <17681090387@163.com>
2015-05-30 16:09:57 +0800

    新闻详情页个人登录信息显示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,11 +1,32 @@
-from flask import render_template
+from flask import render_template, current_app, session
 
+from info import constants
+from info.models import News, User
 from info.modules.news import news_blu
 
 @news_blu.route("/<int:news_id>")
 def news_detail(news_id):
+    user_id = session.get('user_id', None)
+    user = None
+    if user_id:
+        try:
+            user = User.query.get(user_id)
+        except Exception as e:
+            current_app.logger.error(e)
+    # 右侧新闻排行逻辑
+    news_list = []
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+    news_dict_li = []
+    # 遍历对象列表，将对象的字典添加到字典列表中
+    for news in news_list:
+        news_dict_li.append(news.to_basic_dict())
     data = {
         # TODO 需要传入数据
+        "news_dict_li":news_dict_li,
+        "user":user.to_dict() if user else None
     }
 
     return render_template("news/detail.html", data=data)
