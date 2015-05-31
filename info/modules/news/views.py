7d1297535d499beece0249a8bbf05fbb829bb682
Hello-Mr-Zhang <17681090387@163.com>
2015-05-31 17:03:40 +0800

    评论新闻和回复评论的后端逻辑

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, current_app, session, g, abort, request, jsonify
 
-from info import constants
-from info.models import News, User
+from info import constants, db
+from info.models import News, User, Comment
 from info.modules.news import news_blu
 from info.utils.common import user_login_data
 from info.utils.response_code import RET
@@ -90,3 +90,45 @@ def news_collect():
             user.collection_news.append(news)
     return jsonify(errno=RET.OK, errmsg="收藏成功")
 
+
+@news_blu.route('/news_comment', methods=["POST"])
+@user_login_data
+def comment_news():
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+    news_id = request.json.get("news_id")
+    comment_content = request.json.get("comment")
+    parent_id = request.json.get("parent_id")
+
+    if not all([news_id, comment_content]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    try:
+        news_id = int(news_id)
+        if parent_id:
+            parent_id = int(parent_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    # 查询新闻，判断新闻是否存在
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="未查询到数据")
+    # 初始化评论模型
+    comment = Comment()
+    comment.user_id = user.id
+    comment.news_id = news_id
+    comment.content = comment_content
+    if parent_id:
+        comment.parent_id = parent_id
+    try:
+        db.session.add(comment)
+        db.seesion.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+    return jsonify(errno=RET.OK, errmsg="OK", comment=comment.to_dict())
