c4517a6c11fd6d4fca0a2d115a9460cea4784a6a
Hello-Mr-Zhang <17681090387@163.com>
2015-06-05 17:24:21 +0800

    个人中心资料显示功能

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,17 +1,30 @@
-from flask import g, render_template, redirect, request
+from flask import g, render_template, redirect, request, jsonify
 
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
 
 
-@profile_blu.route('/base_info')
+@profile_blu.route('/base_info', methods=['get', 'post'])
 @user_login_data
 def base_info():
     user = g.user
     if request.method == "GET":
-        return render_template('news/user_base_info.html', data={"user":user.to_dict()})
+        return render_template('news/user_base_info.html', data={"user": user.to_dict()})
     # 修改用户数据
+    nick_name = request.json.get("nick_name")
+    signature = request.json.get("signature")
+    gender = request.json.get("gender")
 
+    if not all([nick_name, signature, gender]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    if gender not in ['WOMAN', 'MAN']:
+        return jsonify(errno=RET.PARAMERR, errmsg='参数错误')
+    user.signature = signature
+    user.nick_name = nick_name
+    user.gender = gender
+
+    return jsonify(errno=RET.OK, errmsg="OK")
 
 
 @profile_blu.route('/info')
@@ -20,5 +33,5 @@ def info():
     user = g.user
     if not user:
         return redirect('/')
-    data = {"user":user.to_dict()}
+    data = {"user": user.to_dict()}
     return render_template('news/user.html', data=data)
