467f73f54e264544ead045dd12ec44a5750917c2
Hello-Mr-Zhang <17681090387@163.com>
2015-06-05 15:09:37 +0800

    用户中心个人资料的展示

--- info/modules/profile/views.py
+++ info/modules/profile/views.py
@@ -1,7 +1,17 @@
-from flask import g, render_template, redirect
+from flask import g, render_template, redirect, request
 
 from info.modules.profile import profile_blu
 from info.utils.common import user_login_data
+
+
+@profile_blu.route('/base_info')
+@user_login_data
+def base_info():
+    user = g.user
+    if request.method == "GET":
+        return render_template('news/user_base_info.html', data={"user":user.to_dict()})
+    # 修改用户数据
+
 
 
 @profile_blu.route('/info')
